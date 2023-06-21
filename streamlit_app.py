5001af971a2e9f12be6159e8b0b4b51efbeb1fa6
shwetadhimande <136685359+shwetadhimande@users.noreply.github.com>
2023-06-21 19:40:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Avocado")
 streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = streamlit.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
